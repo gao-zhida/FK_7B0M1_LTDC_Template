8a66b1ee70f9dc9f42a9923069476a80c6c078e1
unknown <2263043074@qq.com>
2023-07-07 10:37:34 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/Template.elf.dir/DependInfo.cmake
@@ -0,0 +1,68 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/Job/STM32/FK_7B0M1/LTDC/Core/Startup/startup_stm32h7b0vbtx.s" "E:/Job/STM32/FK_7B0M1/LTDC/cmake-build-debug/CMakeFiles/Template.elf.dir/Core/Startup/startup_stm32h7b0vbtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32H7B0xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32H7xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/Job/STM32/FK_7B0M1/LTDC/Core/Src/gpio.c" "CMakeFiles/Template.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Core/Src/gpio.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Core/Src/ltdc.c" "CMakeFiles/Template.elf.dir/Core/Src/ltdc.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Core/Src/ltdc.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Core/Src/main.c" "CMakeFiles/Template.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Core/Src/main.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Core/Src/stm32h7xx_hal_msp.c" "CMakeFiles/Template.elf.dir/Core/Src/stm32h7xx_hal_msp.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Core/Src/stm32h7xx_hal_msp.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Core/Src/stm32h7xx_it.c" "CMakeFiles/Template.elf.dir/Core/Src/stm32h7xx_it.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Core/Src/stm32h7xx_it.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Core/Src/syscalls.c" "CMakeFiles/Template.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Core/Src/syscalls.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Core/Src/sysmem.c" "CMakeFiles/Template.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Core/Src/sysmem.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Core/Src/system_stm32h7xx.c" "CMakeFiles/Template.elf.dir/Core/Src/system_stm32h7xx.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Core/Src/system_stm32h7xx.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_ltdc.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_ltdc.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_ltdc.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_ltdc_ex.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_ltdc_ex.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_ltdc_ex.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj.d"
+  "E:/Job/STM32/FK_7B0M1/LTDC/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/Template.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
